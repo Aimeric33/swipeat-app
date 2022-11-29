83854b19e6099e65e004b2c21f0f6c8d751b3afe
georges ryssen <georges.ryssen@gmail.com>
2022-11-29 15:48:25 +0100

    1st version of seed

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,11 +1,12 @@
 class Meal < ApplicationRecord
   belongs_to :restaurant
-  has_many :favorites
-  has_many :bookings
+  has_many :favorites, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :description, presence: true, length: { maximum: 500 }
   validates :price, presence: true
   validates :photo, presence: true
+  validates :type, presence: true
 end
