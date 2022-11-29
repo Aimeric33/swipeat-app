315353e84d2f79e53b066ee13d307ab51fca7d75
georges ryssen <georges.ryssen@gmail.com>
2022-11-29 12:21:00 +0100

    model favorite

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_28_164949) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_29_104653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.boolean "like"
+    t.boolean "superlike"
+    t.boolean "eaten"
+    t.bigint "user_id", null: false
+    t.bigint "meal_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["meal_id"], name: "index_favorites_on_meal_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "meals", force: :cascade do |t|
     t.string "name"
@@ -51,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_28_164949) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favorites", "meals"
+  add_foreign_key "favorites", "users"
   add_foreign_key "meals", "restaurants"
   add_foreign_key "restaurants", "users"
 end
