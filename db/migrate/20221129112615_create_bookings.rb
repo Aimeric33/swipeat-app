d306fe65b6de6f6bacd8e323213e30f82eca033d
Aimeric Marchau <marchauaimeric@gmail.com>
2022-11-29 12:32:23 +0100

    creation of models and associations

---
+++ db/migrate/20221129112615_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.date :date
+      t.references :user, null: false, foreign_key: true
+      t.references :meal, null: false, foreign_key: true
+      t.integer :number_people
+
+      t.timestamps
+    end
+  end
+end
