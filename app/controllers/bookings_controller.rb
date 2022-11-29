3e0771767a682842800073e0a7f8088d48107cb8
Aimeric Marchau <marchauaimeric@gmail.com>
2022-11-29 14:55:11 +0100

    bookings controller with methods and bookings index

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,41 @@
+class BookingsController < ApplicationController
+  before_action :set_booking, only: :show
+  before_action :set_meal, only: %i[new create]
+
+  def index
+    @bookings = Booking.where(user_id: current_user)
+  end
+
+  def show
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.meal = @meal
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def set_meal
+    @meal = Meal.find(params[:meal_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :number_people)
+  end
+end
