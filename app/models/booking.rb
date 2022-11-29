03270939ae5429f7141d44ce288131f718a9640e
Aimeric Marchau <marchauaimeric@gmail.com>
2022-11-29 13:14:52 +0100

    layout for sign-in and sign-up pages

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :meal
+
+  validates :date, presence: true
+  validates :number_people, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
