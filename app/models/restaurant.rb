8be3f5514aa81d6b12d4e12c3d16b0847296874e
Charlene Nicol <charln.nicol@gmail.com>
2022-11-29 14:25:21 +0100

    dependent destroy added

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  has_many :meals
+  has_many :meals, dependent: :destroy
 
   validates :name, :address, :phone, presence: true, uniqueness: true
   validates :name, length: { minimum: 3 }
