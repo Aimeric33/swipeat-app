2e45bef985ca40c067e8ec626968c7819885f8bf
Antoine Berson <antoine.berson01@gmail.com>
2022-11-29 12:59:47 +0100

    model controller ok

---
+++ app/controllers/meals_controller.rb
@@ -0,0 +1,49 @@
+class MealsController < ApplicationController
+  before_action :set_pull, only: %i[show edit update destroy]
+
+  def index
+    @meals = Meal.all
+  end
+
+  def show
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+    @meal.user = current_user
+    if @meal.save
+      redirect_to meal_path(@meal)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @meal.update(meal_params)
+
+    redirect_to meal_path(@meal)
+  end
+
+  def destroy
+    @meal.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def meal_params
+    params.require(:meal).permit(:name, :type, :description, :price, :photo)
+  end
+
+  def set_pull
+    @meal = Meal.find(params[:id])
+  end
+end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -2,4 +2,10 @@ class Meal < ApplicationRecord
   belongs_to :restaurant
   has_many :favorites
   has_many :bookings
+  has_one_attached :photo
+
+  validates :name, presence: true, length: { minimum: 2 }
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :price, presence: true
+  validates :photo, presence: true
 end
