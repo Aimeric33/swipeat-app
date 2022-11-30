628b6d24bb7ed1286de9d61862cea2258c55b468
Antoine Berson <antoine.berson01@gmail.com>
2022-11-30 13:05:48 +0100

    views ok, show de my_meals?

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,4 +1,7 @@
 class Meal < ApplicationRecord
+  CATEGORY = ["italien", "pizzas", "indien", "burgers", "kebab", "asiatique",
+              "sushis", "coréen", "thaï", "poke", "végétarien", "vegan", "halal", "mexicain"]
+
   belongs_to :restaurant
   has_many :favorites, dependent: :destroy
   has_many :bookings, dependent: :destroy
@@ -6,6 +9,7 @@ class Meal < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 2 }
   validates :description, presence: true, length: { maximum: 500 }
+
   validates :price, presence: true
   validates :photo, presence: true
   validates :category, presence: true
