af6777f93174d41d993a770083b27e41a3c70d24
georges ryssen <georges.ryssen@gmail.com>
2022-11-30 16:57:10 +0100

    add links to navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :set_restaurant_owner
 
   def after_sign_in_path_for(resource)
     if resource.owner
@@ -15,4 +16,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email first_name last_name owner address])
   end
+
+  # def set_restaurant_owner
+  #   @restaurant = Restaurant.where(user_id: current_user.id)
+  # end
 end
